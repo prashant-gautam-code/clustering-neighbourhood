6e72f8aa2c31b137217a1b9169054154da108dc2
prashant-gautam-code <53464115+prashant-gautam-code@users.noreply.github.com>
2019-08-20 11:49:33 +0530

    Capstone Week4 Applied Data Science

---
+++ CraftBeer.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 17 12:47:16 2019
+
+@author: I030038
+"""
+
+
+import requests
+import pandas as pd
+import numpy as np
+
+# import k-means from clustering stage
+from sklearn.cluster import KMeans
+
+# Matplotlib and associated plotting modules
+import matplotlib.cm as cm
+import matplotlib.colors as colors
